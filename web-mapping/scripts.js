2b79a150e16bc8417987bd1e75939c5a14aa411f
defrank1 <134450565+defrank1@users.noreply.github.com>
2024-03-28 19:34:14 -0400

    port js fiddle into html, css, and js files

---
+++ web-mapping/scripts.js
@@ -0,0 +1,15 @@
+$('.button-56').on('click', function() {
+    $('body').css('background-color', 'gold')
+  })
+  
+  $('.button-59').on('click', function() {
+    $('body').css('background-color', 'lightcoral')
+  })
+  
+  $('.button-57').on('click', function() {
+    $('body').css('background-color', 'lightskyblue')
+  })
+  
+  $('.button-58').on('click', function() {
+    $('body').css('background-color', 'lightgreen')
+  })
