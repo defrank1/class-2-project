0113890de557bafa0aecdfa39e637758ba61daab
defrank1 <134450565+defrank1@users.noreply.github.com>
2024-04-11 15:56:30 -0400

    added some more descriptive comments to the JS

--- scripts.js
+++ scripts.js
@@ -1,25 +1,25 @@
-// Each Button is coded below:
+// This tells the page which information to display when you click on a button
 var service_introduced_year
 $('button').on('click', function () {
   var classList = $(this).attr("class");
   console.log(classList)
 
-  // Red:
+  // These are the details for the Red button:
   if (classList === 'red-button') {
-    service_introduced_year = "1976"
-    number_of_stations = "27"
-    distance_in_miles = "32"
-    line_termini = "Shady Grove to Glenmont"
+    service_introduced_year = "1976"   // Year service was introduced on this line
+    number_of_stations = "27" // the number of stations the line serves
+    distance_in_miles = "32" // the distance to the nearest mile of the line track
+    line_termini = "Shady Grove to Glenmont" // the two terminal stations on the line
 
 
-    $('body').css('background-color', 'crimson')
-    $('.right-column').css('color', 'white')
+    $('body').css('background-color', 'crimson') // background color after you click
+    $('.right-column').css('color', 'white') // text color after you click
     $('.service_introduced').text(service_introduced_year)
     $('.number_of_stations').text(number_of_stations)
     $('.distance_in_miles').text(distance_in_miles)
     $('.line_termini').text(line_termini)
   }
-  // Orange:
+  // These are the details for the Orange button:
   if (classList === 'orange-button') {
     service_introduced_year = "1978"
     number_of_stations = "26"
@@ -34,7 +34,7 @@ $('button').on('click', function () {
     $('.distance_in_miles').text(distance_in_miles)
     $('.line_termini').text(line_termini)
   }
-  // Blue:
+  // These are the details for the Blue button:
   if (classList === 'blue-button') {
     service_introduced_year = "1977"
     number_of_stations = "28"
@@ -50,7 +50,7 @@ $('button').on('click', function () {
     $('.line_termini').text(line_termini)
   }
 
-  // Green:
+  // These are the details for the Green button:
   if (classList === 'green-button') {
     service_introduced_year = "1991"
     number_of_stations = "21"
@@ -65,7 +65,7 @@ $('button').on('click', function () {
     $('.distance_in_miles').text(distance_in_miles)
     $('.line_termini').text(line_termini)
   }
-  // Yellow:
+  // These are the details for the Yellow button:
   if (classList === 'yellow-button') {
     service_introduced_year = "1983"
     number_of_stations = "13"
@@ -80,7 +80,7 @@ $('button').on('click', function () {
     $('.distance_in_miles').text(distance_in_miles)
     $('.line_termini').text(line_termini)
   }
-  // Silver:
+  // These are the details for the Silver button:
   if (classList === 'silver-button') {
     service_introduced_year = "2014"
     number_of_stations = "34"
